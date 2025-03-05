23b7901cd2950b1e0bbc28ea11c40129983e786a
SHINJUNGSUN <sjs920818@gmail.com>
2025-03-05 23:56:45 +0900

    배포 설정 및 기본 구조 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://sjs818.notion.site/Sunny-1927b148d13380fc8d73c114c4c5ee60">
+            About Me 😁
           </Link>
         </div>
       </div>
@@ -32,9 +32,7 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
